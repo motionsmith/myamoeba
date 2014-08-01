f372a8a5d5d3a0cd50543e3cf95e375dfb0f7cbc
unknown <EricS@VHQ-EricS-1.corp.vnw.com>
2014-08-01 11:43:58 -0700

    You can now breed and rename amoebae.

--- www/app/scripts/controllers/main.js
+++ www/app/scripts/controllers/main.js
@@ -2,12 +2,55 @@
 
 angular.module('myAmoebaApp')
     .controller('MainCtrl', ["$scope", "$location", 'MyAmoebaUser', 'Amoeba', '$rootScope', function ($scope, $location, MyAmoebaUser, Amoeba, $rootScope) {
-        //$scope.myAmoebae = [{"name": "Loading....................................."}];
         $scope.ownerAmoebaeLoaded = false;
+        
         $scope.handleLogoutClick = function() {
             MyAmoebaUser.logOut();
             $location.url('/login');
         };
+        
+        $scope.handleBreedClick = function() {
+            if ($scope.selectedAmoeba == $scope.breedWithAmoeba) {
+                $scope.breedStateMessage = 'Amoebae don\'t breed asexually.';   
+            } else {
+                $scope.breedStateMessage = 'Breeding...';
+            }
+            
+            var parentA = $scope.selectedAmoeba;
+            var parentB = $scope.breedWithAmoeba;
+            var babyAmoeba = new Amoeba();
+            babyAmoeba.set('name', 'Baby of ' + parentA.name + ' and ' + parentB.name);
+            babyAmoeba.set('owner', $rootScope.sessionUser);
+            babyAmoeba.set('breeder', $rootScope.sessionUser);
+            babyAmoeba.set('parentA', parentA);
+            babyAmoeba.set('parentB', parentB);
+            $scope.myAmoebae.push(babyAmoeba);
+            $scope.selectedAmoeba = babyAmoeba;
+            babyAmoeba.save(null, {
+                success: function(babyAmoebaAgain) {
+                    $scope.breedStateMessage = 'My stars! A new amoeba is born!';
+                    $scope.$apply();
+                },
+                error: function(babyAmoebaAgain, error) {
+                    $scope.breedStateMessage = error.message;
+                    $scope.$apply();
+                }
+            });
+        };
+        
+        $scope.handleRenameClick = function() {
+            var amoebaToRename = $scope.selectedAmoeba;
+            amoebaToRename.save(null, {
+                success: function(amoebaAgain) {
+                    $scope.renameStateMessage = "Name changed!";
+                    $scope.$apply();
+                },
+                error: function(amoebaAgain, error) {
+                    $scope.renameStateMessage = error.message;
+                    $scope.$apply();
+                }
+            });
+        }
         
         if ($rootScope.sessionUser) {
              //Get the amoebae that this user owns.
@@ -19,6 +62,7 @@ angular.module('myAmoebaApp')
                 success: function(results) {
                     $scope.myAmoebae = results;
                     $scope.selectedAmoeba = $scope.myAmoebae[0];
+                    $scope.breedWithAmoeba = $scope.myAmoebae[0];
                     $scope.ownerAmoebaeLoaded = true;
                     $scope.$apply();
                 },
