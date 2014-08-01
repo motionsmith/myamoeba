cc74ea9ebf42c991f68eee78bbf4a5c15d3840c5
chrisdoehring <chrisdoehring@gmail.com>
2014-08-01 10:16:00 -0700

    Adding template for getAncestors function.

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -4,3 +4,35 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("nodes", function(request, response) {
+	var query = new Parse.Query("Node");
+        query.include("parents");
+	query.find({
+		success: function (results) {
+			response.success(results);
+		}
+		,
+		error: function() { 
+			response.error("Something went wrong.");
+		}
+	});
+});
+
+Parse.Cloud.define("getAncestors", function(request, response) {
+	
+	var query = new Parse.Query("Node");
+	query.equalTo("objectId", request.params.nodeId);
+    query.include("parents");
+    query.include("owner");
+	query.find({
+		success: function (results) {
+			response.success(results);
+		}
+		,
+		error: function() { 
+			response.error("Something went wrong.");
+		}
+	});
+});
+
