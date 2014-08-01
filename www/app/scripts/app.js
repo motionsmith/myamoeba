f6d746b828b8fae20bd6a8ded3d88fcf05a2d844
unknown <EricS@VHQ-EricS-1.corp.vnw.com>
2014-08-01 10:53:07 -0700

    You can now select an owned amoeba and look at data about it.

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -7,9 +7,10 @@ angular
         'ngResource',
         'ngRoute',
         'ngSanitize',
-        'ngTouch'
+        'ngTouch',
+        'MyAmoebaModels'
     ])
-    .config(function ($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         .when('/', {
             templateUrl: 'views/main.html',
@@ -22,4 +23,29 @@ angular
         .otherwise({
             redirectTo: '/'
         });
-    });
+    }])
+    .run(['$rootScope', '$location', 'MyAmoebaUser', function($rootScope, $location, MyAmoebaUser) {
+        Parse.initialize("iLnBp9fs6Rt8bT4aFWZdiVShVs0fZuxhbpyh20UX", "UCNtTlpwG06wLcXBhY8YwlScUndCt3jKsx4VHY6H");
+        
+        $rootScope.sessionUser = MyAmoebaUser.current();
+        
+        window.fbAsyncInit = function() {
+              console.log("Facebook SDK downloaded. Attempting to initialize Facebook...");
+              console.log("Initializing Facebook.");
+              Parse.FacebookUtils.init({
+                  appId         : '266061040264102',
+                  cookie        : true,
+                  xfbml         : true,
+                  version       : 'v1.0'
+              });
+          };
+          
+          console.log("Page load. Loading Facebook SDK.");
+          (function(d, s, id){
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) {return;}
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_US/all.js";
+              fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+    }]);
--- www/app/scripts/controllers/main.js
+++ www/app/scripts/controllers/main.js
@@ -1,18 +1,32 @@
 'use strict';
 
 angular.module('myAmoebaApp')
-    .controller('MainCtrl', ["$scope", "$location", function ($scope, $location) {
-        var currentUser = Parse.User.current();
-        
+    .controller('MainCtrl', ["$scope", "$location", 'MyAmoebaUser', 'Amoeba', '$rootScope', function ($scope, $location, MyAmoebaUser, Amoeba, $rootScope) {
+        //$scope.myAmoebae = [{"name": "Loading....................................."}];
+        $scope.ownerAmoebaeLoaded = false;
         $scope.handleLogoutClick = function() {
-            Parse.User.logOut();
-            currentUser = Parse.User.current();
+            MyAmoebaUser.logOut();
             $location.url('/login');
         };
         
-        
-        if (currentUser) {
-            $scope.userFirstName = currentUser.get("firstName");
+        if ($rootScope.sessionUser) {
+             //Get the amoebae that this user owns.
+            var myAmoebaeQuery = new Parse.Query(Amoeba);
+            myAmoebaeQuery.equalTo("owner", $rootScope.sessionUser);
+            myAmoebaeQuery.include("owner");
+            myAmoebaeQuery.include("breeder");
+            myAmoebaeQuery.find({
+                success: function(results) {
+                    $scope.myAmoebae = results;
+                    $scope.selectedAmoeba = $scope.myAmoebae[0];
+                    $scope.ownerAmoebaeLoaded = true;
+                    $scope.$apply();
+                },
+                error: function(error) {
+                    console.log(error.message);
+                }
+            });
+            
         } else {
             console.log("Routing to login screen...");
             $location.url("/login");
