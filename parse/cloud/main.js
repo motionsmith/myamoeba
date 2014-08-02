eddf534281b0050de9ff52cb5b92db3a80a403f6
chris doehring <chrisdoehring@gmail.com>
2014-08-02 10:36:52 -0700

    Updated trigger on Amoeba save.

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -139,33 +139,42 @@ Parse.Cloud.define("getAncestors", function(request, response) {
 Parse.Cloud.beforeSave("Amoeba", function(request, response) {
 
 	parentKeys = ['parentA', 'parentB'];
-    totalFriends = request.object.get('totalFriends');
-    if (isNaN(totalFriends)) {
-    	totalFriends = 0;
-    }
-    numAncestors = 0;
+	breeder = request.object.get('breeder');
+	console.log("Saving Amoeba: Breeder: "+JSON.stringify(breeder));
 
-	for (k in parentKeys) {
-		var parent = request.object.get(parentKeys[k]);
 
-		parent_totalAncestralFriends = 0;
-		parent_numAncestors = 0;
-		if (parent) {
-			numAncestors += 1; // count parent
-			parentAncestors = parent.get('numAncestors');
-			if (parentAncestors) { // count parent-ancestors
-				numAncestors += parentAncestors;
-			}
+    breeder.fetch().then(function(breeder) {
 
-			parentTotalFriends = parent.get('totalFriends');
-			if (parentTotalFriends) {
-				totalFriends += parentTotalFriends;
+    	totalFriends = breeder.get('numFriends');
+	    console.log("Saving Amoeba: " + request.object.id + ", totalFriends: " + totalFriends);
+	    if (isNaN(totalFriends)) {
+	    	totalFriends = 0;
+	    }
+	    numAncestors = 0;
+
+		for (k in parentKeys) {
+			var parent = request.object.get(parentKeys[k]);
+
+			parent_totalAncestralFriends = 0;
+			parent_numAncestors = 0;
+			if (parent) {
+				numAncestors += 1; // count parent
+				parentAncestors = parent.get('numAncestors');
+				if (parentAncestors) { // count parent-ancestors
+					numAncestors += parentAncestors;
+				}
+
+				parentTotalFriends = parent.get('totalFriends');
+				if (parentTotalFriends) {
+					totalFriends += parentTotalFriends;
+				}
 			}
 		}
-	}
 
-	request.object.set("numAncestors", numAncestors);
-	request.object.set("totalFriends", totalFriends);
+		request.object.set("numAncestors", numAncestors);
+		request.object.set("totalFriends", totalFriends);
 
-	response.success();
+		response.success();
+
+    });
 });
