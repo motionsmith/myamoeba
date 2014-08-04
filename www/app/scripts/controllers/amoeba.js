5ecd7d576ca8b48ca9b00c7479067e65f7fac983
Eric Smith <motionsmith@gmail.com>
2014-08-04 13:45:35 -0700

    Adding ancestors/descendents data to amoeba details.

--- www/app/scripts/controllers/amoeba.js
+++ www/app/scripts/controllers/amoeba.js
@@ -5,6 +5,7 @@ angular.module('myAmoebaApp')
 
         $scope.pageReady = false;
 
+        //Get info about this amoeba.
         var amoebaQuery = new Parse.Query(Amoeba);
         amoebaQuery.equalTo('objectId', $routeParams.amoeba);
         amoebaQuery.include('owner');
@@ -21,5 +22,29 @@ angular.module('myAmoebaApp')
                 $scope.amoebaStatusMessage = "A problem happened: " + error.message;
                 $scope.$apply();
             }
-        })
+        });
+
+        //Get this amoebae's ancestors
+        Parse.Cloud.run('getAncestors', {"amoebaId": $routeParams.amoeba}, {
+            success: function(result) {
+                $scope.ancestors = result.ancestors;
+                $scope.$apply();
+            },
+            error: function(result, error) {
+                $scope.amoebaStatusMessage = "Problem getting ancestors: " + error.message;
+                $scope.$apply();
+            },
+        });
+
+        //Get this amoebae's descendents
+        Parse.Cloud.run('getOffspring', {"amoebaId": $routeParams.amoeba}, {
+            success: function(result) {
+                $scope.descendents = result;
+                $scope.$apply();
+            },
+            error: function(result, error) {
+                $scope.amoebaStatusMessage = "Problem getting offspring: " + error.message;
+                $scope.$apply();
+            },
+        });
     }]);
