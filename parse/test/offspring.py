efe1380e780d55f7843bffb604ed489032290fe0
chris doehring <chrisdoehring@gmail.com>
2014-08-04 22:38:11 -0700

    Adding offspring adhoc test.

---
+++ parse/test/offspring.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import json, httplib
+
+
+AUTH_HEADERS = {
+    "X-Parse-Application-Id": "iLnBp9fs6Rt8bT4aFWZdiVShVs0fZuxhbpyh20UX",
+    "X-Parse-REST-API-Key": "O0aoMOzkCjHAzmSeTHTv22n2gSonI69FTf8pjHr2"
+}
+
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
+connection.request('POST', '/1/functions/getOffspring', json.dumps({'amoebaId':'SagFwkV3eZ'}),
+ 	{
+       "X-Parse-Application-Id": "iLnBp9fs6Rt8bT4aFWZdiVShVs0fZuxhbpyh20UX",
+       "X-Parse-REST-API-Key": "O0aoMOzkCjHAzmSeTHTv22n2gSonI69FTf8pjHr2",
+       "Content-Type": "application/json"
+     })
+result = json.loads(connection.getresponse().read())
+print json.dumps(result, indent=2)
